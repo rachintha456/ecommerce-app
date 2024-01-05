c2fb0cd10e033346a077b6669290bc7387de6de3
rachintha456 <rachinthaviranga456@gmail.com>
2024-01-05 22:33:02 +0530

    2

---
+++ src/app/services/order.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {finalize, Observable} from "rxjs";
+import {AngularFirestore, DocumentChangeAction} from "@angular/fire/compat/firestore";
+import {AngularFireStorage} from "@angular/fire/compat/storage";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor() { }
+
+
+}
+
